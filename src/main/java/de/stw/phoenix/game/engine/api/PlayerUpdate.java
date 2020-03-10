75ffa70b7ff0ac33d6c1d63bf3ec3fa130a9a118
Markus von Rüden <m.v.rueden@open-email.de>
2020-03-10 09:42:42 +0100

    Next Update

--- src/main/java/de/stw/phoenix/game/engine/api/PlayerUpdate.java
+++ src/main/java/de/stw/phoenix/game/engine/api/PlayerUpdate.java
@@ -1,6 +1,9 @@
 package de.stw.phoenix.game.engine.api;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 public interface PlayerUpdate {
 
+    @JsonIgnore
     GameBehaviour getBehaviour();
 }
