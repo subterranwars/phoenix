b4f095e58e87a5167cea05a7b397c14a3a3d2819
Markus von Rüden <m.v.rueden@open-email.de>
2020-03-18 21:05:01 +0100

    Fix import order and identation

--- src/main/java/de/stw/phoenix/game/engine/impl/GameEventCompletionHandler.java
+++ src/main/java/de/stw/phoenix/game/engine/impl/GameEventCompletionHandler.java
@@ -1,11 +1,5 @@
 package de.stw.phoenix.game.engine.impl;
 
-import java.security.SecureRandom;
-import java.time.Instant;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
 import com.google.common.eventbus.Subscribe;
 import de.stw.phoenix.game.engine.construction.api.ConstructionEvent;
 import de.stw.phoenix.game.engine.construction.api.ConstructionInfo;
@@ -17,6 +11,11 @@ import de.stw.phoenix.game.player.api.GameEvent;
 import de.stw.phoenix.game.player.api.ImmutablePlayer;
 import de.stw.phoenix.game.player.api.Notification;
 import de.stw.phoenix.game.player.api.PlayerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.security.SecureRandom;
+import java.time.Instant;
 
 @Service
 public class GameEventCompletionHandler {
@@ -27,31 +26,31 @@ public class GameEventCompletionHandler {
 
     @Subscribe
     public void onEventCompletion(GameEvent event) {
-	final Notification notification = event.accept(new EventVisitor<Notification>() {
+		final Notification notification = event.accept(new EventVisitor<Notification>() {
 
-	    @Override
-	    public Notification visit(ConstructionEvent constructionEvent) {
-		ConstructionInfo info = constructionEvent.getConstructionInfo();
-		return new Notification(secureRandom.nextLong(), Instant.now(), "Construction completed",
-			info.getBuilding().getLabel() + " Lvl. " + info.getLevelToBuild());
-	    }
+			@Override
+			public Notification visit(ConstructionEvent constructionEvent) {
+			ConstructionInfo info = constructionEvent.getConstructionInfo();
+			return new Notification(secureRandom.nextLong(), Instant.now(), "Construction completed",
+				info.getBuilding().getLabel() + " Lvl. " + info.getLevelToBuild());
+			}
 
-	    @Override
-	    public Notification visit(ResourceSearchEvent resourceSearchEvent) {
-		return new Notification(secureRandom.nextLong(), Instant.now(), "Resource found",
-			resourceSearchEvent.getResource().getName());
-	    }
+			@Override
+			public Notification visit(ResourceSearchEvent resourceSearchEvent) {
+			return new Notification(secureRandom.nextLong(), Instant.now(), "Resource found",
+				resourceSearchEvent.getResource().getName());
+			}
 
-		@Override
-		public Notification visit(ResearchEvent researchEvent) {
-			final ResearchInfo researchInfo = researchEvent.getResearchInfo();
-			return new Notification(secureRandom.nextLong(), Instant.now(), "Research completed",
-					researchInfo.getResearch().getLabel() + " Lvl. " + researchInfo.getLevelToResearch());
-		}
-	});
-	ImmutablePlayer player = playerService.get(event.getPlayerRef().getId());
-	playerService.modify(player, (mutablePlayer) -> {
-	    mutablePlayer.addNotification(notification);
-	});
+			@Override
+			public Notification visit(ResearchEvent researchEvent) {
+				final ResearchInfo researchInfo = researchEvent.getResearchInfo();
+				return new Notification(secureRandom.nextLong(), Instant.now(), "Research completed",
+						researchInfo.getResearch().getLabel() + " Lvl. " + researchInfo.getLevelToResearch());
+			}
+		});
+		ImmutablePlayer player = playerService.get(event.getPlayerRef().getId());
+		playerService.modify(player, (mutablePlayer) -> {
+			mutablePlayer.addNotification(notification);
+		});
     }
 }
